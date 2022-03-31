0e5e951bbae8453d7f43afb7e7995fd172e975f7
tcvdh <tcvdh@outlook.com>
2022-04-01 00:21:17 +0200

    ethers.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,12 @@
 import { whitelistAddresses, addresses } from './merkle.js?ver=2.1.0'
 import { ABI } from './abi.js';
 
-
-// Moralis load
-const serverUrl = "https://gg7nilrspbep.usemoralis.com:2053/server";
-const appId = "gH2426wZlJIXypjOokoU96cVpDocAqR3t71NX9ln";
-Moralis.start({ serverUrl, appId });
+ const Web3Modal = window.Web3Modal.default;
+ const WalletConnectProvider = window.WalletConnectProvider.default;
+ const infuraProvider = new ethers.providers.JsonRpcProvider("https://polygon-mainnet.infura.io/v3/a0ecf0217614452099724b8999730684");
+ const mainnetProvider = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/a0ecf0217614452099724b8999730684");
+ const infuraContract = new ethers.Contract("0xf034ADa7450C426E2cCaEF995d7aA226a45f7B80", ABI, infuraProvider)
+ var contract;
 
 
 // login
@@ -13,58 +14,69 @@ var walletAddress = "0x";
 getTotalSupply();
 
 async function loginWeb3() {
-  const isMetaMaskInstalled= await Moralis.isMetaMaskInstalled();
-  if(isMetaMaskInstalled === false) {
-    document.getElementById('claimable').innerHTML = "You don't have MetaMask installed...";
-    return
-  }
-  // ethers.js
-  const web3 = await Moralis.enableWeb3();
-//   await Moralis.enableWeb3();
-// const web3 = new Web3(Moralis.provider);
-  const chainId = await Moralis.getChainId();
-  if(chainId != 137) {
-    const newChainId = 137;
-    const chainIdHex = await Moralis.switchNetwork(newChainId);
-  }
-  // const getAccounts = await web3.eth.getAccounts();
-  // ethers.js
-  const getAccounts = await web3.listAccounts();
-  walletAddress = getAccounts[0];
-  const ethFirst = walletAddress.substring(0,4);
-  const ethLast = walletAddress.substring(38,42);
-  const fullAddress = `${ethFirst}...${ethLast}`;
-  document.getElementById('connect').innerHTML = fullAddress;
-  start();
-}
+  // console.log("hello")
 
-async function loginWalletConnect() {
-  const web3 = await Moralis.enableWeb3({ provider: "walletconnect", chainId: 137 });
-  // const getAccounts = await web3.eth.getAccounts();
-  const getAccounts = await web3.listAccounts();
-  walletAddress = getAccounts[0];
-  const ethFirst = walletAddress.substring(0,4);
-  const ethLast = walletAddress.substring(38,42);
-  const fullAddress = `${ethFirst}...${ethLast}`;
+  const providerOptions = {
+    walletconnect: {
+      package: WalletConnectProvider,
+      options: {
+        infuraId: "a0ecf0217614452099724b8999730684",
+        rpc: {137: "https://polygon-mainnet.infura.io/v3/a0ecf0217614452099724b8999730684"},
+      }
+    },
+
+  };
+
+  let web3Modal = new Web3Modal({
+    cacheProvider: false, // optional
+    providerOptions, // required
+    disableInjectedProvider: false, // optional. For MetaMask / Brave / Opera.
+  });
+  const instance = await web3Modal.connect();
+  var provider = new ethers.providers.Web3Provider(instance);
+  
+  
+  const currentChain = await provider.getNetwork()
+  if(currentChain.chainId != 137) {
+    // console.log("hello")
+    await window.ethereum.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: '0x89' }], // chainId must be in hexadecimal numbers
+    });
+    provider = new ethers.providers.Web3Provider(instance);
+  }
+  const signer = provider.getSigner();
+  contract = new ethers.Contract("0xf034ADa7450C426E2cCaEF995d7aA226a45f7B80", ABI, signer)
+
+  walletAddress = await signer.getAddress();
+  // var fullAddress;
+
+  // const hasENS = await mainnetProvider.lookupAddress(walletAddress);
+
+  // if(hasENS) {
+  //   fullAddress = hasENS
+  // } else {
+    const ethFirst = walletAddress.substring(0,4);
+    const ethLast = walletAddress.substring(38,42);
+    const fullAddress = `${ethFirst}...${ethLast}`;
+  // }
+
+
+  
   document.getElementById('connect').innerHTML = fullAddress;
-  start();
+  if(new Date().getTime() >= 1648821600000) {
+    start();
+  } else(startFree())
 }
 
 
 async function getTotalSupply() {
-    const options = {
-      chain: "matic",
-      address: "0xf034ADa7450C426E2cCaEF995d7aA226a45f7B80",
-      function_name: "totalSupply",
-      abi: ABI,
-    };
-  
-    const totalSupply = await Moralis.Web3API.native.runContractFunction(options);
-    document.getElementById('totalSupply').innerHTML = `Minted: ${totalSupply}/500`;
+    const totalSupply = await infuraContract.totalSupply()
+    document.getElementById('totalSupply').innerHTML = `Minted: ${totalSupply}/10000`;
   }
 
 
-  async function mint() {
+  async function mintFree() {
     const leafNodes = whitelistAddresses.map(addr => addr);
     const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true});
     var found = addresses.lastIndexOf(walletAddress);
@@ -77,28 +89,30 @@ async function getTotalSupply() {
     var claimingAddress = leafNodes[found];
     const hexProof = merkleTree.getHexProof(claimingAddress);
 
-      const options = {
-          contractAddress: "0xf034ADa7450C426E2cCaEF995d7aA226a45f7B80",
-          functionName: "freeMintPhase",
-          abi: ABI,
-          params: {
-            _to: walletAddress,
-            index: found.toString(),
-            proof: hexProof
-          },
-        };
-      
-        const receipt = await Moralis.executeFunction(options);
+        const receipt = await contract.freeMintPhase(walletAddress, found, hexProof);
         console.log(receipt);
-        await start();
+        await startFree();
     
+  }
+
+  async function mint() {
+    var amount = parseInt(document.getElementById('amountNumber').value);
+    const singlePrice = await contract.price();
+    const totalPrice = amount * singlePrice;
+
+    var overrideOptions = {
+      value: totalPrice
+    }
+
+    const receipt = await contract.mint(walletAddress, amount, overrideOptions);
+    console.log(receipt);
+    await start();
   }
 
 
 async function main() {
-    const isWeb3Active = Moralis.ensureWeb3IsInstalled()
-    if (!isWeb3Active) {
-      $("#subscribeModal").modal('toggle');
+    if (!contract) {
+      await loginWeb3();
       return
     }
     
@@ -107,12 +121,25 @@ async function main() {
       await mint();
       await getTotalSupply();  
     }
+
+    if(document.getElementById('main').innerHTML === "MINT FREE") {
+      await mintFree();
+    }
   }
 
 
 
-
 async function start() {
+  getTotalSupply();
+  document.getElementById('amounttext').innerHTML = `Please select the amount you want to mint:`;
+  document.getElementById("amount").style.visibility = "visible";
+  document.getElementById("cost").style.visibility = "visible";
+  document.getElementById("amounttext").style.visibility = "visible";
+  document.getElementById('main').innerHTML = "MINT";
+}
+
+
+async function startFree() {
   getTotalSupply();
   var found = addresses.lastIndexOf(walletAddress);
   if(found < 0) {
@@ -121,17 +148,8 @@ async function start() {
     document.getElementById("amounttext").style.visibility = "visible";
     return
   }
-  const options1 = {
-    chain: "matic",
-    address: "0xf034ADa7450C426E2cCaEF995d7aA226a45f7B80",
-    function_name: "checkClaimed",
-    abi: ABI,
-    params: {
-      index: found.toString()
-    }
-  };
+  const claimed = await contract.checkClaimed(found);
 
-  const claimed = await Moralis.Web3API.native.runContractFunction(options1);
   if(claimed === true) {
     document.getElementById('amounttext').innerHTML = `You are not on the free mint whitelist`;
     $('#amounttext').addClass('text-danger');
@@ -141,14 +159,14 @@ async function start() {
   }
   document.getElementById('amounttext').innerHTML = `You can mint 2 free TTC NFTs`;
   document.getElementById("amounttext").style.visibility = "visible";
-  document.getElementById('main').innerHTML = "MINT";
+  document.getElementById('main').innerHTML = "MINT FREE";
 }
 
 
 
 
 document.getElementById("main").onclick = main;
-document.getElementById("btn-login-metamask").onclick = loginWeb3;
-document.getElementById("btn-login-coinbase").onclick = loginWeb3;
-document.getElementById("btn-login-trustwallet").onclick = loginWeb3;
-document.getElementById("btn-login-walletconnect").onclick = loginWalletConnect;
+// document.getElementById("btn-login-metamask").onclick = loginWeb3;
+// document.getElementById("btn-login-coinbase").onclick = loginWeb3;
+// document.getElementById("btn-login-trustwallet").onclick = loginWeb3;
+// document.getElementById("btn-login-walletconnect").onclick = loginWalletConnect;
